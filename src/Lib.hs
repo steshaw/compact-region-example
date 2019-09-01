336439fb350a8b1e50fd1280b70ddb00b4571f42
Steven Shaw <steven@steshaw.org>
2019-09-01 13:12:09 +1000

    ✨ Read/write compact file

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,22 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE TypeApplications #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib (main) where
+
+import Data.Compact
+import Data.Compact.Serialize
+import System.Directory (doesFileExist)
+import Control.Monad
+
+main :: IO ()
+main = do
+  let file = "flintstones.compact"
+  exists <- doesFileExist file
+  unless exists $ do
+    putStrLn $ "Writing " <> file
+    orig_c <- compact ("Fred Flintstone", ["Wilma", "Barney"])
+    writeCompact file orig_c
+  putStrLn $ "Reading " <> file
+  res <- unsafeReadCompact @(String, [String]) file
+  case res of
+    Left err -> fail err
+    Right c -> print (getCompact c)
